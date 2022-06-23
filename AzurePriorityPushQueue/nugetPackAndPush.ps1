b77c07b64c994b745f8af8f610d83305e5b5a7e8
Union Palenshus <palenshus@gmail.com>
2022-06-22 18:56:50 -0700

    Upgrading to .NET Standard

--- AzurePriorityPushQueue/nugetPackAndPush.ps1
+++ AzurePriorityPushQueue/nugetPackAndPush.ps1
@@ -1,5 +1,11 @@
 param([Parameter(Mandatory=$true)] $ApiKey)
 
-del *.nupkg
-nuget pack .\AzurePriorityPushQueue.csproj
-nuget push *.nupkg $ApiKey -Source https://www.nuget.org/api/v2/package
+$xml = [xml](gc .\AzurePriorityPushQueue.csproj)
+$packageId = $xml.Project.PropertyGroup[1].PackageId
+$version = $xml.Project.PropertyGroup[1].Version
+$target = "$packageId.$version.nupkg"
+
+del $target -ErrorAction Ignore
+dotnet pack --configuration Release --output .
+
+dotnet nuget push $target --api-key $ApiKey --source https://api.nuget.org/v3/index.json
---
+++ Tests/Helpers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    internal static class Helpers
+    {
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source)
+        {
+            return source.OrderBy(x => Guid.NewGuid());
+        }
+    }
+}
